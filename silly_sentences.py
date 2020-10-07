85f126a0f64553a2244491b697ed560f029d132e
Angel Gonzalez <angel.gonzalez@daviestech.org>
2020-10-07 19:49:11 +0000

    fixed errors

--- silly_sentences.py
+++ silly_sentences.py
@@ -1,16 +1,17 @@
 print ("Let's play Silly Sentences!")
-name = input("enter a name: ")
+n = input("enter a name: ")
 place = input("enter a place: ")
 adjective1 = input ("Enter a adjective: ")
-adjective2 = input(" enter an adjective: ")
+adjective2 = input("Enter an adjective: ")
 food1 = input ("enter a food: ")
 food2 = input (" enter a food: ")
 verb = input (" enter a verb: ")
 adverb = input (" enter an adverb: ")
 noun = input (" enter a noun: ")
-print=(name + " was planning a dream vacation to " + place + ".") 
-print=(name + " was especially looking forward to trying the local cuisine, including " + adjective1 + "." + food1 + " and " + food2 + ".")
 
-print=(name + " will have to practice the language " + adverb + " to make it easier to" + verb + " with people. ") 
+print (n + " was planning a dream vacation to " + place + ".") 
+print (n + " was especially looking forward to trying the local cuisine, including " + adjective1 + "." + food1 + " and " + food2 + ".")
 
-print=(name + "  has a long list of sights to see, including the " + noun + " museum and the " + adjective2 + " park. ") 
+print (n + " will have to practice the language " + adverb + " to make it easier to" + verb + " with people. ") 
+
+print (n + "  has a long list of sights to see, including the " + noun + " museum and the " + adjective2 + " park. ") 
